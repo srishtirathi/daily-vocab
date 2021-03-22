62eda0948088fceaad2e8670f81a52765873ac13
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-03-22 08:53:13 +0530

    test: add tdd to post vocab service

--- daily-vocab-be/src/services/__test__/vocab.service.test.js
+++ daily-vocab-be/src/services/__test__/vocab.service.test.js
@@ -1,4 +1,4 @@
-const { getAllVocab } = require('../vocab.service');
+const { getAllVocab, createVocab } = require('../vocab.service');
 const { Vocab } = require('../../models');
 
 describe('getAllVocab Function', () => {
@@ -11,5 +11,32 @@ describe('getAllVocab Function', () => {
     spyOnFindAll.mockResolvedValue(expectedValue);
     const recievedData = await getAllVocab();
     expect(recievedData).toEqual(expectedValue);
+  });
+});
+
+describe('createVocab Function', () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+  const MOCK_TODO = {
+    word: 'hi',
+    sentence: 'hi srishti',
+  };
+
+  const spyOnCreate = jest.spyOn(Vocab, 'create');
+  it('should append data to database', async () => {
+    const MOCK_RESOLVED_VALUE = {
+      dataValues: {
+        id: '1',
+        word: 'hi',
+        sentence: 'hi srishti',
+      },
+    };
+
+    spyOnCreate.mockResolvedValueOnce(MOCK_RESOLVED_VALUE);
+
+    const todo1 = await createVocab(MOCK_TODO);
+
+    expect(todo1).toEqual(MOCK_RESOLVED_VALUE.dataValues);
   });
 });
