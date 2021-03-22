0d916140de21a751c6178864a0ca1160076b405d
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-03-22 08:56:46 +0530

    test: add tdd to post vocab handler

--- daily-vocab-be/src/handlers/__test__/vocab.handler.test.js
+++ daily-vocab-be/src/handlers/__test__/vocab.handler.test.js
@@ -1,5 +1,5 @@
 const {
-  getAllVocab,
+  getAllVocab, postVocabHandler,
 } = require('../vocab.handler');
 const vocabServices = require('../../services/vocab.service');
 
@@ -38,6 +38,44 @@ describe('get(/vocab) Handler', () => {
   it('should set status code to 500 if error in accessing db', async () => {
     spyOnGetAllTodo.mockRejectedValue(new Error('Error in accessing data'));
     await getAllVocab(mockRequestObject, mockResponseObject);
+    expect(mockResponseObject.status).toHaveBeenCalledWith(500);
+  });
+});
+
+describe('post(/vocab) Handler', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  const mockSend = jest.fn();
+  const mockResponseObject = {
+    status: jest.fn(() => ({ send: mockSend })),
+  };
+  const mockRequestObject = {
+    body: {
+      word: 'hi',
+      sentence: 'hi srishti',
+    },
+
+  };
+  const MOCK_EXPECT_TODO = {
+    id: '1',
+    word: 'hi',
+    sentence: 'hi srishti',
+  };
+  const spyOnCreateVocab = jest.spyOn(vocabServices, 'createVocab');
+  it('should set response status code to 201 on successful append of vocab', async () => {
+    spyOnCreateVocab.mockResolvedValue(MOCK_EXPECT_TODO);
+    await postVocabHandler(mockRequestObject, mockResponseObject);
+    expect(mockResponseObject.status).toHaveBeenCalledWith(201);
+  });
+  it('should return the created todo on successful append', async () => {
+    spyOnCreateVocab.mockResolvedValue(MOCK_EXPECT_TODO);
+    await postVocabHandler(mockRequestObject, mockResponseObject);
+    expect(mockResponseObject.status().send).toHaveBeenCalledWith(MOCK_EXPECT_TODO);
+  });
+  it('should set status code to 500 if any error occurs in accessing db', async () => {
+    spyOnCreateVocab.mockRejectedValue(new Error('Error accessing db'));
+    await postVocabHandler(mockRequestObject, mockResponseObject);
     expect(mockResponseObject.status).toHaveBeenCalledWith(500);
   });
 });
